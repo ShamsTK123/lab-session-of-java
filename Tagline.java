f5c443befd5f12817a1b4a63f24f4e0da71d348e
ShamsTK123 <88656603+ShamsTK123@users.noreply.github.com>
2022-08-29 07:10:21 -0100

    Add files via upload

---
+++ Tagline.java
@@ -0,0 +1,28 @@
+ /*
+Name - Shams Tabrej Khan
+Date - 18 Aug 2022
+Program - Question no.2
+*/ 
+class Tagline
+{
+	static void BrandName()
+	{
+		String tagline = "Adidas"; //Putting Brand Name
+		switch (tagline)
+		{
+			case "Adidas" : System.out.println ("Impossible is Nothing");
+				break;
+			case "Nike" :System.out.println ("Just Do It");
+				break;
+			case "Reebok" : System.out.println("I am what I am");
+				break;
+			case "Puma" : System.out.println ("FOREVER FASTER IS ALWAYS FIRST, NEVER SECOND, NEVER THIRD");
+				break;
+			case "Sketchers" : System.out.println ("Comfort and bounce in every step");
+				break;
+		}
+	}
+	public static void main (String args[])
+	{
+		BrandName();
+	}
